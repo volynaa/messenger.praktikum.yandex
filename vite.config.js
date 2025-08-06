71d676eebd923b66993fe2864f375c3f004dc707
volynaa <volynskiy.nik@mail.ru>
2025-08-06 15:59:12 +0300

    Sprint 1

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
-    server: {
-        port: 3000
-    },
     build: {
         rollupOptions: {
             input: {
