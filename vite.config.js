812c9dd3fe4c862a43a844cd58716be3518986eb
volynaa <volynskiy.nik@mail.ru>
2025-08-06 16:12:39 +0300

    Sprint 1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
     build: {
@@ -12,14 +11,5 @@ export default defineConfig({
     },
     css: {
         postcss: './postcss.config.js'
-    },
-    plugins: [
-        handlebars({
-            partialDirectory: './src/components',
-            compileOptions: {
-                strict: true,
-                noEscape: true,
-            },
-        }),
-    ],
+    }
 })
