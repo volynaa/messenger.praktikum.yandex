8a36d82e441fccc4117c826985aca5d734e28833
volynaa <volynskiy.nik@mail.ru>
2025-08-06 15:55:13 +0300

    Sprint 1

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000
-app.use(express.static('./dist'))
+const PORT = 3000;
 
-app.listen(PORT, ()=> `Server started on port ${PORT}`)
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
