c3e74b0b91d51a87e4ee2a2f93bdae3a32c688fe
volynaa <volynskiy.nik@mail.ru>
2025-09-16 19:18:23 +0300

    Sprint 2

---
+++ src/types/raw.d.ts
@@ -0,0 +1,14 @@
+declare module '*.hbs?raw' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.html?raw' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.txt?raw' {
+    const content: string;
+    export default content;
+}
