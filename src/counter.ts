d3c4f60748d53303c6e495e434daf7d090ffe50f
volynaa <volynskiy.nik@mail.ru>
2025-09-16 19:15:22 +0300

    Sprint 2

--- src/counter.ts
+++ src/counter.ts
@@ -1,6 +1,6 @@
-export function setupCounter(element : HTMLButtonElement) {
+export function setupCounter(element: HTMLButtonElement): void {
   let counter = 0;
-  const setCounter = (count) => {
+  const setCounter = (count: number) => {
     counter = count;
     element.innerHTML = `count is ${counter}`;
   };
