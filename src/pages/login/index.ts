3b1c30eabf4d3947ffabe414db3ed084a9bcae24
volynaa <volynskiy.nik@mail.ru>
2025-09-23 13:39:54 +0300

    Sprint 2

--- src/pages/login/index.ts
+++ src/pages/login/index.ts
@@ -12,7 +12,7 @@ export default class Login extends Block {
   constructor() {
     super('div', {
       events: {
-        focusout: (e: Event) => this.handleSubmit(e),
+        blur: (e: Event) => this.handleSubmit(e),
         submit: (e: Event) => this.handleSubmit(e),
         click: (e: Event) => this.handleButtonClick(e)
       }
@@ -46,8 +46,10 @@ export default class Login extends Block {
       console.error('Form validation initialization error:', error);
     }
   }
+  private handleBlur(e: Event): void {
+    console.log('yes')
+  }
   private handleSubmit(e: Event): void {
-    console.log('ter')
     e.preventDefault();
 
     if (!this.validator) {
