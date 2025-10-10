63c0bc29a18ecd592c0d65ccc19dc0cc6f433c2e
volynaa <volynskiy.nik@mail.ru>
2025-10-10 09:55:05 +0300

    Sprint 3

--- src/api/base-api.ts
+++ src/api/base-api.ts
@@ -3,7 +3,7 @@ import {HTTPTransport} from "../ui/fetch";
 const chatAPIInstance = new HTTPTransport('https://ya-praktikum.tech/api/v2/');
 
 export default class BaseAPI {
-    post<T>(url: string, options: T = undefined) {
+    post<T>(url: string, options: T = undefined as T) {
         try {
             return chatAPIInstance.post(url, {data: options});
         }
